a2374d421dcf1ca51456a40c23d456b9fd0cfd3e
CatrielCastellanoBalbis <82192142+CatrielCastellanoBalbis@users.noreply.github.com>
2021-11-28 11:08:39 -0300

    Update main.c

--- main.c
+++ main.c
@@ -11,34 +11,30 @@ int main(){
 	 	case 1:
 	 		
 	 		semaforo = &rojo_seg;
-	        (*semaforo)(datos.tiempo_seguridad);
-	        break;
+	                (*semaforo)(datos.tiempo_seguridad);
+	                break;
 	        
-	    case 2:
+	         case 2:
 	    	
-	    	semaforo = &amarillo1;
-	        (*semaforo)(datos.tiempo_A);
-	        break;
+	    	        semaforo = &amarillo1;
+	                (*semaforo)(datos.tiempo_A);
+	                break;
 	       
-	    case 3:
+	         case 3:
 	 		
 	 		semaforo = &verde1;
-	        (*semaforo)(datos.tiempo_1V);
-	        break;
+	                (*semaforo)(datos.tiempo_1V);
+	                break;
 	        
-	    case 4:
+	         case 4:
 	    	
-	    	semaforo = &verde2;
-	        (*semaforo)(datos.tiempo_2V);
-	        break;
+	    	        semaforo = &verde2;
+	                (*semaforo)(datos.tiempo_2V);
+	                break;
 	 	
 	 };
 	 
     };
-    
-    return 0;
-    
-}
     
     return 0;
     
