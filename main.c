9b14c49fd6b3ec690402fb393543b8462e4aa2c8
CatrielCastellanoBalbis <82192142+CatrielCastellanoBalbis@users.noreply.github.com>
2021-11-28 11:07:08 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,37 +2,43 @@
 
 int main(){
 	
-	void (*semaforo)(int);
+	int (*semaforo)(int);
 	
 	while(1){
 		
-	 semaforo = &rojo_seg;
-	 (*semaforo)(tiempo_seguridad);
-	 
-	 semaforo = &amarillo1;
-	 (*semaforo)(tiempo_A);
-	 
-	 semaforo = &verde1;
-	 (*semaforo)(tiempo_1V);
-	 
-	 semaforo = &amarillo1;
-	 (*semaforo)(tiempo_A);
-	 
-     semaforo = &rojo_seg;
-	 (*semaforo)(tiempo_seguridad);
-	 
-	 semaforo = &amarillo2;
-	 (*semaforo)(tiempo_A);
-	 
-	 semaforo = &verde2;
-	 (*semaforo)(tiempo_2V);
-	 
-	 semaforo = &amarillo2;
-	 (*semaforo)(tiempo_A);
-	 
-	 
+	 switch(retorno){
+	 	
+	 	case 1:
+	 		
+	 		semaforo = &rojo_seg;
+	        (*semaforo)(datos.tiempo_seguridad);
+	        break;
+	        
+	    case 2:
+	    	
+	    	semaforo = &amarillo1;
+	        (*semaforo)(datos.tiempo_A);
+	        break;
+	       
+	    case 3:
+	 		
+	 		semaforo = &verde1;
+	        (*semaforo)(datos.tiempo_1V);
+	        break;
+	        
+	    case 4:
+	    	
+	    	semaforo = &verde2;
+	        (*semaforo)(datos.tiempo_2V);
+	        break;
+	 	
+	 };
 	 
     };
+    
+    return 0;
+    
+}
     
     return 0;
     
